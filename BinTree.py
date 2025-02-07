61374888458a8d7ca6bfdfd78a6580175ef5865d
Sahithyan04 <sahithyan316@gmail.com>
2025-02-07 23:55:27 +0530

    updated a new file

---
+++ BinTree.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self ,data):
+        self.data = data
+        self.left = None
+        self.right = None
+class BinTree:
+    def __init__(self ,data):
+        node = Node(data)
+        self.root = node
+    def InOrder(self ,node):
+        if node :
+            self.InOrder(node.left)
+            print(node.data,end= '->')
+            self.InOrder(node.right)
+    def PostOrder(self ,node):
+        if node :
+            self.PostOrder(node.left)
+            self.PostOrder(node.right)
+            print(node.data,end= '->')
+    def PreOrder(self ,node):
+        if node :
+            print(node.data,end= '->')
+            self.PreOrder(node.left)
+            self.PreOrder(node.right)
+
+tre = BinTree(2)
+tre.root.left = Node(8)
+tre.root.right = Node(7)
+tre.root.left.left = Node(4) 
+tre.root.left.right = Node(3)
+tre.InOrder(tre.root)
+print("\n")
+tre.PostOrder(tre.root)
+print("\n")
+tre.PreOrder(tre.root)
+'''             2
+           8        7
+        4     3   '''  
