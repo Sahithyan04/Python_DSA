ccb8534348c2ee18706fd829b0370147e657c9f1
Sahithyan04 <sahithyan316@gmail.com>
2025-02-12 21:47:22 +0530

    added BreathFirstSearch function to BinTree

--- BinTree.py
+++ BinTree.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Node:
     def __init__(self ,data):
         self.data = data
@@ -22,12 +23,26 @@ class BinTree:
             print(node.data,end= '->')
             self.PreOrder(node.left)
             self.PreOrder(node.right)
+    def BreathFirstSearch(self ,node):
+        opt_stack = deque()
+        if node:
+            opt_stack.append(self.root)
+        while opt_stack:
+            opt = opt_stack.popleft()
+            print(opt.data,end="->")
+            if opt.left :
+                opt_stack.append(opt.left)
+            if opt.right :
+                opt_stack.append(opt.right)
+        
 
 tre = BinTree(2)
 tre.root.left = Node(8)
 tre.root.right = Node(7)
 tre.root.left.left = Node(4) 
 tre.root.left.right = Node(3)
+tre.BreathFirstSearch(tre.root)
+print("\n")
 tre.InOrder(tre.root)
 print("\n")
 tre.PostOrder(tre.root)
